dde8010de75045d51052c00d6f5ed529e6398757
Abhiram Sankar <abhiramsankar102000@gmail.com>
2025-06-12 01:43:52 -0400

    Updated key

--- expressServer/server.js
+++ expressServer/server.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 // app.use(bodyParser.json());
 
-const KEY_BYTES = crypto.randomBytes(32); // 256‐bit key
+const KEY_BYTES = crypto.randomBytes(64); // 256‐bit key
 
 // Encrypt
 // Body: { plaintext: "<ASCII string>" }
